1fa94baf049261ef76776dbadc10f6de58f135bf
sungch1798 <154121562+sungch1798@users.noreply.github.com>
2023-12-26 16:38:44 +0800

    Ver 1.0 + color

--- src/main/java/com/example/mytempsite/pd/productcontroller.java
+++ src/main/java/com/example/mytempsite/pd/productcontroller.java
@@ -1,2 +1,27 @@
-package com.example.mytempsite.pd;public class productcontroller {
+package com.example.mytempsite.pd;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.apache.coyote.Request;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static jakarta.servlet.http.HttpSession.*;
+
+@Controller
+public class productcontroller {
+    @GetMapping("/product")
+    public String showpd(){
+        return "product-page";
+    }
+
+    @GetMapping("/product1")  //代表網誌上的連結
+    public String showD(Model m01, HttpSession session, HttpServletRequest request) {
+        session.setAttribute("msg1","透過session 傳遞的資料，需要session.msg1");
+        request.setAttribute("msg1","透過request 傳遞資料，不需要request.msg1");
+        m01.addAttribute("msg2","系統預設抓取Model和Request值");
+
+        return "product-page";  //導向的網站名稱
+    }
 }
