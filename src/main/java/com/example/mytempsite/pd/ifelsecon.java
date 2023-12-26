076878de0dedc7a052a81693646325161139e249
sungch1798 <154121562+sungch1798@users.noreply.github.com>
2023-12-26 16:38:44 +0800

    Ver 1.0 + color

--- src/main/java/com/example/mytempsite/pd/ifelsecon.java
+++ src/main/java/com/example/mytempsite/pd/ifelsecon.java
@@ -1,2 +1,27 @@
-package com.example.mytempsite.pd;public class ifelsecon {
+package com.example.mytempsite.pd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+
+@Controller
+public class ifelsecon {
+    @GetMapping("/if")
+    public String showif(Model model){
+        model.addAttribute("Amdin",true);
+        return "ifelse";
+    }
+    @GetMapping("/car01")
+    public String carli(Model model){
+        ArrayList<String> products =new ArrayList<>();
+        products.add("Toyota Corolla");
+        products.add("Ford Explorer");
+        products.add("Honda Accord");
+        products.add("BMW X5");
+        products.add("Chevrolet Malibu");
+        model.addAttribute("cars",products);
+        return "carpd";
+    }
 }
